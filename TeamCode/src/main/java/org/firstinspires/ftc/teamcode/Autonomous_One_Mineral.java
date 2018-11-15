47bf9d2b2da1b887777fc6f44072c0157c1862a3
Bert Bee-Lindgren <bert@bee-lindgren.com>
2018-11-15 18:06:40 -0500

    [Erik, Louis, Rose, Leo] We added a left turn block and worked on a new autonomous program to push a mineral

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous_One_Mineral.java
@@ -0,0 +1,49 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "A: Land", group = "Tinkering")
+
+public class Autonomous_Land extends AutonomousOpMode
+{
+        @Override
+        void teamInit()
+        {
+                super.teamInit();
+                robot.calibrateHook();
+        }
+
+        @Override
+        void teamRun()
+        {
+//                robot.hookUp(1, true);
+//                robot.getImuHeading();
+//                double startHeading = robot.getImuHeading();
+//                double endHeading = startHeading - 45;
+//                double degreesToGo = 45;
+//                robot.getRightMotor().setPower(0);
+//                robot.getLeftMotor().setPower(0.5);
+//                while(shouldOpModeKeepRunning() && robot.getImuHeading() > endHeading);
+//                {
+//                    degreesToGo = robot.getImuHeading() - endHeading;
+//
+//                    setStatus(String.format("%. 1f degrees To Go ", degreesToGo));
+//                }
+//                robot.hookDown(1, false);
+//                robot.inchmove( 20, 0.5);
+//                robot.stop();
+
+                robot.hookUp(1, false);
+                robot.turnRight(15, 1);
+                robot.hookDown(1, false);
+                robot.inchmove( 10, 0.4);
+                robot.stop();
+                robot.turnRight(345, 1);
+                robot.inchmove(30, 1);
+                robot.setArmExtensionPower(1);
+                teamSleep(2000);
+                robot.stop();
+                robot.hookDown(1, true);
+
+        }
+}
